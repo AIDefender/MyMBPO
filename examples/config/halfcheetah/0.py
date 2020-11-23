49a7971573c6a71f080257cd6b77d4309a7614e5
Zhenghai Xue <xuezh@smail.nju.edu.cn>
2020-11-23 21:40:38 +0800

    minor changes

--- examples/config/halfcheetah/0.py
+++ examples/config/halfcheetah/0.py
@@ -10,8 +10,7 @@ params = {
     'kwargs': {
         'epoch_length': 1000,
         'train_every_n_steps': 1,
-        'n_train_repeat': 40,
-        # 'n_train_repeat': 1,
+        'n_train_repeat': 10,
         'critic_train_repeat': 1,
         'eval_render_mode': None,
         'eval_n_episodes': 1,
