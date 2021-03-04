f6b2ed27852abdb4d0607df65f0e8e180e3d3090
Zhenghai Xue <xuezh@smail.nju.edu.cn>
2021-03-04 15:50:47 +0800

    rename mbpo_experiment

---
+++ examples/config/continous_grid/sac.py
@@ -0,0 +1,37 @@
+params = {
+    'type': 'MBPO',
+    'universe': 'gym',
+    'domain': 'ContinuousGrid',
+    'task': 'v0',
+
+    'log_dir': '~/ray_mbpo/',
+    'exp_name': 'defaults',
+
+    'kwargs': {
+        'epoch_length': 100,
+        'train_every_n_steps': 1,
+        'actor_train_repeat': 1,
+        'critic_train_repeat': 1,
+        'eval_render_mode': None,
+        'eval_n_episodes': 10,
+        'eval_deterministic': True,
+
+        'discount': 0.99,
+        'tau': 5e-3,
+        'reward_scale': 1.0,
+
+        'model_train_freq': 250,
+        'model_retain_epochs': 1,
+        'rollout_batch_size': 100e3,
+        'sample_repeat': 1, # repeatedly propose actions on one start state
+        'deterministic': False,
+        'num_networks': 7,
+        'num_elites': 5,
+        'real_ratio': 1,
+        'critic_same_as_actor': True,
+        'target_entropy': -1,
+        'max_model_t': None,
+        'rollout_schedule': [20, 150, 1, 15],
+    }
+}
+
