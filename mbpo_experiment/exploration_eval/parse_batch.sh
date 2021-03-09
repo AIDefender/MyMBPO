35c643b1b20c017818dfab4662e0bf355bbc2209
Zhenghai Xue <xuezh@smail.nju.edu.cn>
2021-03-09 08:12:05 +0800

    add ploting for inter and cross group q std

--- mbpo_experiment/exploration_eval/parse_batch.sh
+++ mbpo_experiment/exploration_eval/parse_batch.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
-for index in {5..85..5}
+for index in {5..40..5}
 	do
 		echo checkpoint $index
 		#python parse.py -d grid_sac3x3_data -i $index
+		#python parse.py -d grid_sac3x3distinct_data -i $index
 		#python parse.py -d grid_sac_data -i $index
-		python parse.py -d reacher_sac_data -i $index
+		python parse.py -d reacher_sac3x3distinct_data -i $index
 done
 
 
