8c971b1d6430b0f843885fae4b57032d03d6fe08
Zhenghai Xue <xuezh@smail.nju.edu.cn>
2021-03-12 21:08:58 +0800

    added some config files

--- examples/config/ant/mbpo.py
+++ examples/config/ant/mbpo.py
@@ -1,17 +1,18 @@
 params = {
     'type': 'MBPO',
     'universe': 'gym',
-    'domain': 'HalfCheetah',
+    'domain': 'AntTruncatedObs', ## mbpo/env/ant.py
     'task': 'v2',
 
     'log_dir': '~/ray_mbpo/',
     'exp_name': 'defaults',
+    'seed': 42,
 
     'kwargs': {
         'epoch_length': 1000,
         'train_every_n_steps': 1,
-        'actor_train_repeat': 40,
-        'critic_train_repeat': 40,
+        'actor_train_repeat': 20,
+        'critic_train_repeat': 20,
         'eval_render_mode': None,
         'eval_n_episodes': 10,
         'eval_deterministic': True,
@@ -21,22 +22,18 @@ params = {
         'reward_scale': 1.0,
 
         'model_train_freq': 250,
-        'model_train_slower': 1,
         'model_retain_epochs': 1,
-        'rollout_batch_size': 100e3, # decrease it when debug
         'sample_repeat': 1, # repeatedly propose actions on one start state
+        'rollout_batch_size': 100e3,
         'deterministic': False,
         'num_networks': 7,
         'num_elites': 5,
         'real_ratio': 0.05,
-        # 'actor_real_ratio': 1,
         'critic_same_as_actor': True,
-        'target_entropy': -3,
+        'target_entropy': -4,
         'max_model_t': None,
-        'rollout_schedule': [20, 150, 1, 1],
+        'rollout_schedule': [20, 100, 1, 25],
 
-        'model_load_dir': "/home/linus/ray_mbpo/HalfCheetah/mbpo-model10000/seed:1_2021-03-11_19-36-32im92etgf/models",
-        'model_log_freq': 100,
-        'model_load_index': 80000
+        'model_log_freq': 300,
     }
 }
