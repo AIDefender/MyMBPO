4df8726e3580245aee5b41caa9626a5b42c4659e
Zhenghai Xue <xuezh@smail.nju.edu.cn>
2021-03-13 09:59:41 +0800

    update configs

---
+++ examples/config/ant/mbpo_loadmodel.py
@@ -0,0 +1,41 @@
+params = {
+    'type': 'MBPO',
+    'universe': 'gym',
+    'domain': 'AntTruncatedObs', ## mbpo/env/ant.py
+    'task': 'v2',
+
+    'log_dir': '~/ray_mbpo/',
+    'exp_name': 'defaults',
+    'seed': 42,
+
+    'kwargs': {
+        'epoch_length': 1000,
+        'train_every_n_steps': 1,
+        'actor_train_repeat': 20,
+        'critic_train_repeat': 20,
+        'eval_render_mode': None,
+        'eval_n_episodes': 10,
+        'eval_deterministic': True,
+
+        'discount': 0.99,
+        'tau': 5e-3,
+        'reward_scale': 1.0,
+
+        'model_train_freq': 250,
+        'model_retain_epochs': 1,
+        'sample_repeat': 1, # repeatedly propose actions on one start state
+        'rollout_batch_size': 100e3,
+        'deterministic': False,
+        'num_networks': 7,
+        'num_elites': 5,
+        'real_ratio': 0.05,
+        'critic_same_as_actor': True,
+        'target_entropy': -4,
+        'max_model_t': None,
+        'rollout_schedule': [20, 100, 1, 25],
+
+        'model_log_freq': 300,
+        'model_load_index': 76000,
+        'model_load_dir': "/home/linus/ray_mbpo/AntTruncatedObs/mbpo-for-model/seed:1_2021-03-12_20-55-5249th27kq/models"
+    }
+}
